fd2ef3d7b27ec67e0d64ab86a5dea6d56ea18a91
letswebtech <letswebtech@gmail.com>
2019-07-20 22:16:46 +0530

    'commit'

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 var PORT = process.env.PORT || 3000 ;
 const http =  require('http');
+
+const bodyParser =  require('body-parser');
+
 const express = require('express');
 
 const app = express();
 
-app.use((req, res)=>{
-    res.send("MY NODE PRJECT ");
+app.get('/add-product',(req, res)=>{
+    
+    res.status(200).send('<form method="POST" action="/add-product"><input type="text" name="message"/><button>Submit</button></form>');
 });
+
+app.post('/add-product',(req, res)=>{
+    console.log('Data send');
+    res.send("Data Saved");
+});
+
+app.get('/',(req, res)=>{
+    res.status(404).send("Page Not Found");
+});
+
 const server = http.createServer(app);
 
 server.listen(PORT);
