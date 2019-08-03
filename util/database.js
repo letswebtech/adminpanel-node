a6229512a3686ef83f307928af9efcfa73ec681d
letswebtech <letswebtech@gmail.com>
2019-08-03 12:13:45 +0530

    'live DB'

--- util/database.js
+++ util/database.js
@@ -1,10 +1,22 @@
+let  PORT = process.env.PORT || 3000 ;
+
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
+const localDB = {
     host: 'localhost',
     user: 'root',
     database: 'testDB',
     password: 'kashif'
-});
+};
+
+const liveDb = {
+    host: 'remotemysql.com',
+    user: 'xb4JJoNeNb',
+    database: 'xb4JJoNeNb',
+    password: 'mWuocDKlu6'
+};
+
+
+const pool = mysql.createPool(PORT == 3000 ? localDB : liveDb);
 
 module.exports = pool.promise();
